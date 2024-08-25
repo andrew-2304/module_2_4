2312029d561bebb67bfec1ebaa1d6161de662b1a
andrew-2304 <henryostwald23@gmail.com>
2024-08-25 23:12:27 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,27 +1,3 @@
-# list_ = ['one', 'two', 'three']
-# list_2 = [2, 3, 4, 5, 1]
-# sum_ = 0
-# for i in range(len(list_2)):
-#     sum_ += list_2[i]
-#
-# print(sum_)
-
-
-
-# for i in range(1, 11):
-#     for j in range(1, 11):
-#         print(f'{i} х {j} = {i * j}')
-
-
-# dict_ = {'a': 1, 'b': 2, 'c': 3}
-# for i, k in dict_.items():
-#     print(i, k)
-
-
-
-
-
-
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
